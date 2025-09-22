fdad8eb32e9acfe5f3900e49a7da581262611fda
Sakashima11 <ju1.sakashima@gmail.com>
2025-09-22 16:48:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 alert('Hello!,JavaScript!');
 //「myBottun」という名前のボタンを見つけて、buttonという変数に保存する
 const button=document.getElementById('myButton');
-
 //そのボタンが「クリック」されたら、alertの命令を実行する
 button.addEventListener('click',()=>{alert('ボタンが押されたよ！');});
