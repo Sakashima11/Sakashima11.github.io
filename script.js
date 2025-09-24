a9a986d24939435c8175cf22c1aeade880e85ce6
Sakashima11 <ju1.sakashima@gmail.com>
2025-09-24 15:12:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-alert('Hello!,JavaScript!');
-//「myBottun」という名前のボタンを見つけて、buttonという変数に保存する
+//HTMLから「myTitle」というIDを持つ要素を探して、title変数に入れる
+const title=document.getElementById('myTitle');
+//ボタンの情報を取得する
 const button=document.getElementById('myButton');
-//そのボタンが「クリック」されたら、alertの命令を実行する
-button.addEventListener('click',()=>{alert('ボタンが押されたよ！');});
-
+//buttonがクリックされたら、以下の命令を実行する
+button.addEventListener('click',()=>{
+  //titleの中身の文章を「ボタンが押されました！」に変更する
+  title.innerHTML='ボタンが押されました！';
+  //以前のalert()のコードはコメントアウトしておきます
+  //alert('ボタンが押されたよ！');});
